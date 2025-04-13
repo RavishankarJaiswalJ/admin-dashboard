df8c3458ec0ed205d945c7ec917e18d94642131f
RavishankarJ <Ravishankarj786@gmail.com>
2025-04-13 11:00:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import AnalyticsPage from './pages/AnalyticsPage'
+import OverviewPage from './pages/OverviewPage'
+import ProductsPage from './pages/ProductsPage'
+import SalesPage from './pages/SalesPage'
+import UsersPage from './pages/UsersPage'
+import OrderPage from './pages/OrdersPage'
+import SettingsPage from './pages/SettingsPage'
+import Sidebar from './components/common/Sidebar'
+
+const App = () => {
+  return (
+   <div className=' flex h-screen bg-gray-900 text-gray-100 overflow-hidden'>
+    <div className='fixed inset-0 z-0'>
+    < div className='absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80'/>
+    <div className='absolute inset-0 backdrop-blur-sm' />
+    </div>
+    <Sidebar/>
+    <Routes>
+      <Route path='/' element={<OverviewPage />}/>
+      <Route path='/products' element={<ProductsPage />}/>
+      <Route path='/users' element={<UsersPage />}/>
+      <Route path='/sales' element={<SalesPage />}/>
+      <Route path='/orders' element={<OrderPage />}/>
+      <Route path='/analytics' element={<AnalyticsPage />}/>
+      <Route path='/settings' element={<SettingsPage />}/>
+      
+    </Routes>
+    
+   </div>
+  )
+}
+
+export default App
